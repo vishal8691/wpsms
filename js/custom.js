4cef47b3d60c4956c2bab1c319fbd4f313899faa
vishal8691 <122819765+vishal8691@users.noreply.github.com>
2023-02-23 22:49:52 +0530

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,34 @@ var preloader = $('#preloader');
 // loader function End 
 
 
+
+new SlimSelect({
+  select: '#phoneselect',
+  settings: {
+    showSearch: false,
+  }
+})
+
+
+new SlimSelect({
+  select: '#groupselect',
+  settings: {
+    showSearch: false,
+    placeholderText: 'Select from Group Numbers',
+    maxValuesShown: 20, // Default 20
+    contentLocation: document.getElementById('text')
+  },
+})
+
+new SlimSelect({
+  select: '#groupselect2',
+  settings: {
+    showSearch: false,
+    placeholderText: 'Enter Individual Phone Numbers',
+    maxValuesShown: 20, // Default 20
+    contentLocation: document.getElementById('text')
+  },
+})
+
+
+
